77457e4bdaa28918e989cece4ec80cb5739bb289
danielpmedac <daniel.perez@doc.medac.es>
2025-01-13 10:33:23 +0100

    Creacion del proyecto

---
+++ EjercicioEvaluable5_Interfaces/src/main/java/com/mycompany/ejercicioevaluable5_interfaces/EjercicioEvaluable5_Interfaces.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicioevaluable5_interfaces;
+
+/**
+ *
+ * @author danie
+ */
+public class EjercicioEvaluable5_Interfaces {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
